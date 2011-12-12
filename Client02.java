cda01619832b45072401769b597149c7f57fe8d8
ma-ya <ma-ya@ht.sfc.keio.ac.jp>
2011-12-12 15:49:41 +0900

    Update Client02.java

---
+++ Client02.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client02 {
+
+  public static final String HOST = "localhost";
+	public static final int PORT = 10389;
+
+	public static void main(String[] args) {
+		try {
+			Socket sock = new Socket(HOST, PORT); // クライアントソケットの作成
+			Scanner sc = new Scanner(sock.getInputStream()); // サーバからのデータ受け取り準備
+			Scanner in = new Scanner(System.in);
+			PrintWriter out = new PrintWriter(sock.getOutputStream(), true); // サーバへのデータ送信
+
+			while (sc.hasNext()) {
+				String ss = sc.nextLine();
+				System.out.println("サーバ：" + ss);
+				if (ss.equals("切断しました")) {
+					break;
+				}
+				ss = in.nextLine();
+				out.println(ss);
+			}
+			sock.close(); // ソケットを閉じる
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+}
